d2166703e7ded0e0acd630d5be069f0ce2ab58a0
MD Ashik <mdashik5360@gmail.com>
2024-11-24 16:28:23 +0600

    url fixed for render.com live

--- src/endpoints/paths.py
+++ src/endpoints/paths.py
@@ -85,6 +85,7 @@ async def upload_file(
 @router.websocket("/ws/{session_id}")
 async def websocket_endpoint(websocket: WebSocket, session_id: str):
     await websocket.accept()
+    print("websocket started.")
 
     #Retrieve session data
     task = active_tasks[session_id]
@@ -126,6 +127,7 @@ async def websocket_endpoint(websocket: WebSocket, session_id: str):
     del active_tasks[session_id]
 
     await websocket.close()
+    print("websocket started.")
 
 @router.get("/download/{file_name}/{translated_text}", response_class=FileResponse)
 async def download_file(file_name: str, background_tasks: BackgroundTasks, translated_text : str):
